284582b77411ba41486344754df1f1cf5f1fb5d1
Stanislav Termosa <termosa.stanislav@gmail.com>
2020-02-11 12:24:47 +0200

    Updates

---
+++ precache-manifest.c2102774dea2cfac410193951d760e0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ea1fc4cd26c491e4352b4336ed57ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2eeffca788a21535759b",
+    "url": "/static/css/main.7ed58023.chunk.css"
+  },
+  {
+    "revision": "eaf2f8a73b057c1049f0",
+    "url": "/static/js/2.d7967d37.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d7967d37.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2eeffca788a21535759b",
+    "url": "/static/js/main.18be5e19.chunk.js"
+  },
+  {
+    "revision": "46f30aee26fdbc59ef5f",
+    "url": "/static/js/runtime-main.f0ad0d7c.js"
+  }
+]);
